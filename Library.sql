a02fb02b3a9caf6a9207932a7868205fdeb7f800
Mark Novak <44879300+Sort617@users.noreply.github.com>
2024-03-08 17:35:55 +0100

    Add files via upload

---
+++ Library.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `Books` (
+  `BookID` integer PRIMARY KEY,
+  `Title` varchar(255),
+  `AuthorID` integer,
+  `GenreID` integer,
+  `PublicationYear` year
+);
+
+CREATE TABLE `Authors` (
+  `AuthorID` integer PRIMARY KEY,
+  `FirstName` varchar(255),
+  `LastName` varchar(255)
+);
+
+CREATE TABLE `Genres` (
+  `GenreID` integer PRIMARY KEY,
+  `Name` varchar(255)
+);
+
+ALTER TABLE `Books` ADD FOREIGN KEY (`AuthorID`) REFERENCES `Authors` (`AuthorID`);
+
+ALTER TABLE `Books` ADD FOREIGN KEY (`GenreID`) REFERENCES `Genres` (`GenreID`);
